49e50f25318d3e9ebcfeb89f3abb7ce4f445d30a
Greg Freed <gregfreed83@gmail.com>
2025-06-26 16:51:58 -0400

    fix(frontend): correct middleware for Vercel build

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,14 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  publicRoutes: ["/sign-in", "/sign-up"]
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    // If the route is not public, then protect it.
+    // This will redirect unauthenticated users to the sign-in page.
+    auth().protect();
+  }
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params.
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|pdf|xml)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
